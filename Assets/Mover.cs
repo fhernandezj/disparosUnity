4e6b64eb75f09077e9a251e0be00a0f4b3cf7232
unknown <franciscohernandez.22@hotmail.com>
2017-08-16 14:22:35 -0500

    Initial commit

---
+++ Assets/Mover.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+	
+	public float movementSpeed = 5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(new Vector3(0f, 1, 0f));
+        }		
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(new Vector3(0f, -1, 0f));
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(new Vector3(0f, 0f, 1) * Time.deltaTime * movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(new Vector3(0f, 0f, -1) * Time.deltaTime * movementSpeed);
+		}
+	}
+}
